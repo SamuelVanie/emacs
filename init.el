8ba946a181ae6caeda8219f89eb2438cd1fdf3c2
SamuelVanie <samuelmichaelvanie@gmail.com>
2023-01-30 02:22:33 +0100

    feat: add ess package for R programming

--- init.el
+++ init.el
@@ -379,6 +379,8 @@
     (setq lsp-java-vmargs '("-XX:+UseParallelGC" "-XX:GCTimeRatio=4" "-XX:AdaptiveSizePolicyWeight=90" "-Dsun.zip.disableMemoryMapping=true" "-Xmx2G" "-Xms100m"))
 )
 
+(use-package ess)
+
 (use-package company
   :after lsp-mode
   :hook (lsp-mode . company-mode)
@@ -418,3 +420,16 @@
 
 (use-package evil-nerd-commenter
   :bind ("M-/" . evilnc-comment-or-uncomment-lines))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(ess yasnippet which-key web-mode use-package typescript-mode rainbow-delimiters prettier org-bullets olivetti no-littering lsp-ui lsp-java lsp-ivy ivy-rich ivy-prescient helpful general forge flycheck evil-nerd-commenter evil-collection emmet-mode doom-themes doom-modeline counsel-projectile company-box auto-package-update all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
