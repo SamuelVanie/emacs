3bbc4c5f464a3159eb273ec5746e55ad156aa821
SamuelVanie <samuelmichaelvanie@gmail.com>
2024-11-20 21:39:48 +0100

    feat: adding copilot-mode to the init.el

--- init.el
+++ init.el
@@ -189,21 +189,16 @@
 
 (use-package hydra) ;; hydra permit to repeat a command easily without repeating the keybindings multiple
 
-(use-package xah-fly-keys
-  :config
-  (xah-fly-keys t)
-  (xah-fly-keys-set-layout "colemak")
-  (setq xah-fly-use-control-key nil)
-  (setq xah-fly-use-meta-key nil)
-  (define-key xah-fly-command-map (kbd "k") 'swiper)
-  (define-key xah-fly-command-map (kbd "SPC b") 'copilot-complete))
+(use-package repeat
+  :ensure nil
+  :hook (after-init . repeat-mode)
+  :custom
+  (repeat-too-dangerous '(kill-this-buffer))
+  (repeat-exit-timeout 5))
 
 (use-package ace-jump-mode
-  :bind (
-         ("C-c SPC" . ace-jump-mode)
-         :map xah-fly-command-map
-         ("SPC z" . ace-jump-mode)
-         ))
+  :bind
+  ("C-c SPC" . ace-jump-mode))
 
 (use-package vterm)
 
@@ -217,7 +212,7 @@
 (use-package doom-themes)
 (use-package ef-themes
   :config
-  (load-theme 'manoj-dark t))
+  (load-theme 'doom-gruvbox t))
 
 (use-package all-the-icons
   :if (display-graphic-p))
@@ -453,6 +448,11 @@
 (use-package wgrep)
 (global-set-key (kbd "C-c r") 'rgrep)
 
+(use-package nix-mode
+  :mode "\\.nix\\'"
+  :config
+  :hook (nix-mode . eglot-ensure))
+
 (use-package direnv
   :config
   (direnv-mode))
@@ -561,6 +561,9 @@
 
 (use-package cider)
 
+(use-package julia-mode
+  :mode "\\.jl\\'")
+
 (use-package company
     :after eglot
     :hook (eglot-managed-mode . company-mode)
@@ -587,111 +590,12 @@
 
 (use-package dockerfile-mode)
 
-(defun rk/copilot-complete-or-accept ()
-    "Command that either triggers a completion or accepts one if one
-is available. Useful if you tend to hammer your keys like I do."
-    (interactive)
-    (if (copilot--overlay-visible)
-        (progn
-        (copilot-accept-completion)
-        (open-line 1)
-        )
-    (copilot-complete)))
-
-(defun rk/copilot-quit ()
-"Run `copilot-clear-overlay' or `keyboard-quit'. If copilot is
-cleared, make sure the overlay doesn't come back too soon."
-(interactive)
-(condition-case err
-    (when copilot--overlay
-        (lexical-let ((pre-copilot-disable-predicates copilot-disable-predicates))
-        (setq copilot-disable-predicates (list (lambda () t)))
-        (copilot-clear-overlay)
-        (run-with-idle-timer
-            1.0
-            nil
-            (lambda ()
-            (setq copilot-disable-predicates pre-copilot-disable-predicates)))))
-    (error handler)))
-
-(defun rk/no-copilot-mode ()
-"Helper for `rk/no-copilot-modes'."
-(copilot-mode -1))
-
-(defvar rk/no-copilot-modes '(shell-mode
-                                inferior-python-mode
-                                eshell-mode
-                                term-mode
-                                vterm-mode
-                                comint-mode
-                                compilation-mode
-                                debugger-mode
-                                dired-mode-hook
-                                compilation-mode-hook
-                                flutter-mode-hook
-                                minibuffer-mode-hook)
-    "Modes in which copilot is inconvenient.")
-
-(defvar rk/copilot-manual-mode nil
-    "When `t' will only show completions when manually triggered, e.g. via M-C-<return>.")
-
-(defvar rk/copilot-enable-for-org nil
-    "Should copilot be enabled for org-mode buffers?")
-
-
-(defun rk/copilot-enable-predicate ()
-    ""
-    (and
-    (eq (get-buffer-window) (selected-window))))
-
-(defun rk/copilot-disable-predicate ()
-    "When copilot should not automatically show completions."
-    (or rk/copilot-manual-mode
-        (member major-mode rk/no-copilot-modes)
-        (and (not rk/copilot-enable-for-org) (eq major-mode 'org-mode))
-        (company--active-p)))
-
-(defun rk/copilot-change-activation ()
-    "Switch between three activation modes:
-    - automatic: copilot will automatically overlay completions
-    - manual: you need to press a key (C-M-<return>) to trigger completions
-    - off: copilot is completely disabled."
-    (interactive)
-    (if (and copilot-mode rk/copilot-manual-mode)
-        (progn
-        (message "deactivating copilot")
-        (global-copilot-mode -1)
-        (setq rk/copilot-manual-mode nil))
-    (if copilot-mode
-        (progn
-            (message "activating copilot manual mode")
-            (setq rk/copilot-manual-mode t))
-        (message "activating copilot mode")
-        (global-copilot-mode))))
-
-
-(straight-use-package '(copilot :host github
-                            :repo "copilot-emacs/copilot.el"
-                            :branch "main"
-                            :files ("dist" "*.el")))
-
-(require 'copilot)
-;; keybindings that are active when copilot shows completions
-(define-key copilot-mode-map (kbd "C-M-<next>") #'copilot-next-completion)
-(define-key copilot-mode-map (kbd "C-M-<prior>") #'copilot-previous-completion)
-(define-key copilot-mode-map (kbd "C-M-<right>") #'copilot-accept-completion-by-word)
-(define-key copilot-mode-map (kbd "C-M-<return>") #'copilot-accept-completion-by-line)
-
-;;global keybindings
-(define-key global-map (kbd "C-M-<down>") #'rk/copilot-complete-or-accept)
-(define-key global-map (kbd "C-M-<escape>") #'rk/copilot-change-activation)
-
-;;Do copilot-quit when pressing C-g
-(advice-add 'keyboard-quit :before #'rk/copilot-quit)
-
-;; ;; deactivate copilot for certain modes
-(add-to-list 'copilot-enable-predicates #'rk/copilot-enable-predicate)
-(add-to-list 'copilot-disable-predicates #'rk/copilot-disable-predicate)
+(use-package copilot
+  :straight (:host github :repo "copilot-emacs/copilot.el" :files ("*.el"))
+  :ensure t)
+  
+(define-key copilot-completion-map (kbd "<tab>") 'copilot-accept-completion)
+(define-key copilot-completion-map (kbd "TAB") 'copilot-accept-completion)
 
 ;; (defun smv/gptel-api-key ()
 ;;   "Retrieve my OpenAI API key from a secure location."
