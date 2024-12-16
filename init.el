7ff204f995be241a34d9f3fba547245deb37b2e3
Samuel Michael Vanie <samuelmichaelvanie@Samuels-MacBook-Air.local>
2024-12-16 17:18:46 +0100

    feat: remove no littering
    - Removed because I prefer my project specific files
    to stays in the same directory as my files.

--- init.el
+++ init.el
@@ -84,17 +84,6 @@
 
 (setq initial-buffer-choice (lambda () (get-buffer "*dashboard*")))
 
-;; NOTE: If you want to move everything out of the ~/.emacs.d folder
-;; reliably, set `user-emacs-directory` before loading no-littering!
-                                        ;(setq user-emacs-directory "~/.cache/emacs")
-
-(use-package no-littering)
-
-;; no-littering doesn't set this by default so we must place
-;; auto save files in the same path as it uses for sessions
-(setq auto-save-file-name-transforms
-      `((".*" ,(no-littering-expand-var-file-name "auto-save/") t)))
-
 (use-package treesit-auto
   :custom
   (treesit-auto-install 'prompt)
