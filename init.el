04c26243560e0d2454b4b87a3875202bd4b1e529
SamuelVanie <samuelmichaelvanie@gmail.com>
2024-12-19 19:55:28 +0100

    fix: remove all-the-icons-completion, don't need it

--- init.el
+++ init.el
@@ -290,12 +290,6 @@
   :init
   (marginalia-mode))
 
-(use-package all-the-icons-completion
-  :after (marginalia all-the-icons)
-  :hook (marginalia-mode . all-the-icons-completion-marginalia-setup)
-  :init
-  (all-the-icons-completion-mode))
-
 (use-package consult
   :init
   (advice-add #'register-preview :override #'consult-register-window)
