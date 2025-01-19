42f7b10047a24111a4aa734745992c40bc56680a
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-01-19 16:57:18 +0100

    feat: add dired popup, so great to have that

--- init.el
+++ init.el
@@ -91,6 +91,21 @@
   (add-hook 'dired-mode-hook #'dired-hide-details-mode)
   (add-hook 'dired-mode-hook #'all-the-icons-dired-mode))
 
+(use-package dired-sidebar
+  :bind (("C-x C-n" . dired-sidebar-toggle-sidebar))
+  :ensure t
+  :commands (dired-sidebar-toggle-sidebar)
+  :init
+  (add-hook 'dired-sidebar-mode-hook
+            (lambda ()
+              (unless (file-remote-p default-directory)
+                (auto-revert-mode))))
+  :config
+  (push 'toggle-window-split dired-sidebar-toggle-hidden-commands)
+  (push 'rotate-windows dired-sidebar-toggle-hidden-commands)
+  (setq dired-sidebar-subtree-line-prefix "__")
+  (setq dired-sidebar-use-term-integration t))
+
 (global-set-key [remap dabbrev-expand] 'hippie-expand)
 
 (use-package dashboard
