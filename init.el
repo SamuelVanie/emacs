b14298c9b1e397e5b60bbeaaf420d4e1eb08c521
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-07-14 12:35:12 -0400

    fix: remove no more existant files

--- init.el
+++ init.el
@@ -1307,9 +1307,7 @@
 
   ;; loads agents
   (load-file (format "%s%s/%s%s" user-emacs-directory "agents" "command_line" ".el"))
-  (load-file (format "%s%s/%s%s" user-emacs-directory "agents" "mayuri_kurotsuchi" ".el"))
   (load-file (format "%s%s/%s%s" user-emacs-directory "agents" "mayuri" ".el"))
-  (load-file (format "%s%s/%s%s" user-emacs-directory "agents" "opencode" ".el"))
   (load-file (format "%s%s/%s%s" user-emacs-directory "agents" "command_line" ".el"))
   (load-file (format "%s%s/%s%s" user-emacs-directory "agents" "summarizer" ".el"))
   (load-file (format "%s%s/%s%s" user-emacs-directory "agents" "summarizer_google" ".el"))
