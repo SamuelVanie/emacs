a2c7a03b7c69b7daf8666fee8ea6abac0b215a47
SamuelVanie <samuelmichaelvanie@gmail.com>
2025-09-23 20:51:12 +0200

    deletion: remove multicursors, no use

--- init.el
+++ init.el
@@ -567,40 +567,6 @@ _~_: tilde         _{_: curly        _*_: asterisks    _s_: custom strings
    "r r"  #'avy-resume
    "r m"  #'avy-move-region))
 
-(use-package multiple-cursors
-  :ensure t
-  :demand t
-  :config
-  (setq mc/cmds-to-run-once '())
-  (defhydra hydra-multiple-cursors (:hint nil)
-    "
- ^Mark^             ^Skip^               ^Edit^
- ^^^^^^^^-----------------------------------------
- _>_: next like this    _i_: to next like this   _+_: edit lines
- _<_: prev like this    _n_: to prev like this   _=_: mark all like this
- _q_: quit
- "
-    ("+" mc/edit-lines)
-    (">" mc/mark-next-like-this)
-    ("<" mc/mark-previous-like-this)
-    ("=" mc/mark-all-like-this)
-    ("i" mc/skip-to-next-like-this)
-    ("n" mc/skip-to-previous-like-this)
-    ("q" nil))
-  (define-key mc/keymap (kbd "<return>") nil)
-  (general-define-key
-   :keymaps '(meow-normal-state-keymap meow-motion-state-keymap)
-   "+" #'hydra-multiple-cursors/body)
-  (setq mc/cmds-to-run-once
-        (append mc/cmds-to-run-once
-                '(hydra-multiple-cursors/body
-                  hydra-multiple-cursors/mc/edit-lines
-                  hydra-multiple-cursors/mc/mark-next-like-this
-                  hydra-multiple-cursors/mc/mark-previous-like-this
-                  hydra-multiple-cursors/mc/mark-all-like-this
-                  hydra-multiple-cursors/mc/skip-to-next-like-this
-                  hydra-multiple-cursors/mc/skip-to-previous-like-this))))
-
 (winner-mode 1) ;; activate the package that recalls the current layout to redo it
 
 (use-package windmove
