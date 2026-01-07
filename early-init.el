03e7a9e0c68d9cb667a9e9d618b7873f467086e4
SamuelVanie <samuelmichaelvanie@gmail.com>
2026-01-07 02:00:22 +0100

    feat: a new early-init file in the spirit of emacs config

---
+++ early-init.el
@@ -0,0 +1,31 @@
+(setq frame-resize-pixelwise t
+      frame-inhibit-implied-resize 'force
+      frame-title-format '("%b")
+      ring-bell-function 'ignore
+      use-file-dialog nil
+      use-short-answers t
+      enable-recursive-minibuffers t
+      inhibit-splash-screen t
+      inhibit-startup-screen t
+      inhibit-x-resources t
+      inhibit-startup-echo-area-message user-login-name ; read the docstring
+      inhibit-startup-buffer-menu t)
+
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq initial-frame-alist `((horizontal-scroll-bars . nil)
+                            (menu-bar-lines . 0)
+                            (tool-bar-lines . 0)
+                            (vertical-scroll-bars . nil)
+                            (border-width . 0)))
+
+
+(defun prot-emacs-no-minibuffer-scroll-bar (frame)
+  "Remove the minibuffer scroll bars from FRAME."
+  (when scroll-bar-mode
+    (set-window-scroll-bars (minibuffer-window frame) nil nil nil nil :persistent)))
+
+(add-hook 'after-make-frame-functions #'prot-emacs-no-minibuffer-scroll-bar)
