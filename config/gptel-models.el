53e0790df05bf658ab4c0592673828ac1b48bb0d
SamuelVanie <samuelmichaelvanie@gmail.com>
2026-03-29 15:37:04 +0200

    feat(models): update openrouter models list

--- config/gptel-models.el
+++ config/gptel-models.el
@@ -18,7 +18,7 @@
   :endpoint "/api/coding/paas/v4/chat/completions"
   :protocol "https"
   :key (with-temp-buffer (insert-file-contents "~/.org/.zai_key") (string-trim (buffer-string)))
-  :models '("glm-5" "glm-4.7" "glm-4-32b-0414-128k" "glm-4.5-air"))
+  :models '("glm-5.1" "glm-4.7" "glm-4-32b-0414-128k" "glm-4.5-air"))
 
 (setq gptel-backend
       (gptel-make-openai "OpenRouter"
@@ -29,20 +29,29 @@
       	:key (with-temp-buffer (insert-file-contents "~/.org/.openr_key") (string-trim (buffer-string)))
       	:models '(
       		  (anthropic/claude-haiku-4.5 :input-cost 1 :output-cost 5)
-      		  (anthropic/claude-sonnet-4.5 :input-cost 3 :output-cost 10)
+      		  (anthropic/claude-sonnet-4.6 :input-cost 3 :output-cost 15)
+      		  (anthropic/claude-opus-4.6 :input-cost 5 :output-cost 25)
+                  
+      		  (xiaomi/mimo-v2-pro :input-cost 1 :output-cost 3)
+                  
       		  (deepseek/deepseek-v3.2 :input-cost 0.224 :output-cost 0.32)
-      		  (google/gemini-2.5-flash-lite :input-cost 0.10 :output-cost 0.4)
-      		  (google/gemini-3-flash-preview :input-cost 0.5 :output-cost 3)
-      		  (google/gemini-3-pro-preview :input-cost 2 :output-cost 12)
+                  
+      		  (google/gemini-3.1-flash-lite-preview :input-cost 0.25 :output-cost 1.5)
+      		  (google/gemini-3.1-pro-preview :input-cost 2 :output-cost 12)
+                  
       		  (minimax/minimax-m2.1 :input-cost 0.3 :output-cost 1.2)
-      		  (moonshotai/kimi-dev-72b :input-cost 0.29 :output-cost 1.15)
-      		  (moonshotai/kimi-k2-thinking :input-cost 0.4 :output-cost 1.75)
+      		  (minimax/minimax-m2.5:free :input-cost 0.0 :output-cost 0.0)
+                  
+      		  (moonshotai/kimi-k2.5 :input-cost 0.42 :output-cost 2.2)
+                  
       		  (openai/gpt-4.1 :input-cost 2 :output-cost 8)
       		  (openai/gpt-5.2 :input-cost 1.75 :output-cost 14)
       		  (openai/gpt-5.1-codex :input-cost 1.25 :output-cost 10)
-      		  (qwen/qwen3-coder :input-cost 0.22 :output-cost 0.95)
+                  
+      		  (qwen/qwen3.5-35b-a3b :input-cost 0.1625 :output-cost 1.3)
       		  (qwen/qwen3-coder-flash :input-cost 0.3 :output-cost 1.50)
       		  (qwen/qwen3-coder-plus :input-cost 1 :output-cost 5)
+                  
       		  (switchpoint/router :input-cost 0.85 :output-cost 3.40)
       		  (x-ai/grok-code-fast-1 :input-cost 0.2 :output-cost 1.5)
       		  (x-ai/grok-4.1-fast :input-cost 0.2 :output-cost 0.5)
