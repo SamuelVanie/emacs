d6ad4b5202f8c4fdff922709e82ddb6def5e1306
SamuelVanie <damsektry@gmail.com>
2026-08-13 16:16:10 +0200

    feat: make sure to load the two new presets

--- config/gptel-custom-functions.el
+++ config/gptel-custom-functions.el
@@ -1,6 +1,10 @@
 ;; loads presets
 (load-file (format "%s%s/%s%s" user-emacs-directory "presets" "command_line" ".el"))
 (load-file (format "%s%s/%s%s" user-emacs-directory "presets" "repomap" ".el"))
+(load-file (format "%s%s/%s%s" user-emacs-directory "presets" "reviewer" ".el"))
+(load-file (format "%s%s/%s%s" user-emacs-directory "presets" "task_summarizer" ".el"))
+
+;; load functions
 (load-file (format "%s%s/%s%s" user-emacs-directory "config" "gptel-refusal-posthook" ".el"))
 (load-file (format "%s%s/%s%s" user-emacs-directory "config" "gptel-notool-minor-mode" ".el"))
 (load-file (format "%s%s/%s%s" user-emacs-directory "config" "gptel-ediff-prehook" ".el"))
