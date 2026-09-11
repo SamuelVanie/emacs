76c8d9b8996e22eb8e0fa8c3ae372fe647929ac9
SamuelVanie <damsektry@gmail.com>
2026-09-11 15:08:37 +0200

    feat: another method for parsing the output

---
+++ utilities/slf4j_jq_prettifier.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+jq -RrC --unbuffered '. as $raw | try fromjson catch $raw' |
+perl -pe '
+  if (/"(?:type|level|severity)"/) {
+    s/\e\[0;32m"(error)"\e\[0m/\e[1;31m"$1"\e[0m/ig;
+    s/\e\[0;32m"(warn(?:ing)?)"\e\[0m/\e[1;33m"$1"\e[0m/ig;
+    s/\e\[0;32m"(info)"\e\[0m/\e[1;32m"$1"\e[0m/ig;
+    s/\e\[0;32m"(debug)"\e\[0m/\e[36m"$1"\e[0m/ig;
+    s/\e\[0;32m"(trace)"\e\[0m/\e[90m"$1"\e[0m/ig;
+  }
+'
